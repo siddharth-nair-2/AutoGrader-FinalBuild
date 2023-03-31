de11c3910517785d54e26a2110c791ebef4506b5
siddharth-nair-2 <nairsid2@gmail.com>
2023-03-31 16:42:56 -0400

    minor fix

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../config/generateToken");
-const User = require("../Models/userModel");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password, userType } = req.body;
